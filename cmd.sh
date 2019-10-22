e180438b9e9e97d78d9589e220a0bb96e7b1177c
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2019-10-22 21:21:02 +0530

    add srilm path

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,11 @@
 # Setting local system jobs (local CPU - no external clusters)
 export train_cmd=run.pl
 export decode_cmd=run.pl
+export cuda_cmd=run.pl
+
+if [[ "$(hostname -f)" == "*.fit.vutbr.cz" ]]; then
+  queue_conf=$HOME/queue_conf/default.conf # see example /homes/kazi/iveselyk/queue_conf/default.conf,
+  export train_cmd="queue.pl --config $queue_conf --mem 2G --matylda 0.2"
+  export decode_cmd="queue.pl --config $queue_conf --mem 3G --matylda 0.1"
+  export cuda_cmd="queue.pl --config $queue_conf --gpu 1 --mem 10G --tmp 40G"
+fi
